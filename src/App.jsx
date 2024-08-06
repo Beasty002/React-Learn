4997f71f49e8acacd02aba1a37d71b38307a6014
Beasty002 <xasbin12@gmail.com>
2024-08-06 20:20:16 +0545

    dynamic values in js

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ export const App = () => {
   return (<div>
     <h1>hello world</h1>
     <Card />
-    <Card />
-    <Card />
     <h2>This is a movie</h2>
   </div >);
 };
 
 
 const Card = () => {    //use PascalCase for naming components
+  const value = "Hey";
   return (
-    <h3>Hello from  componenet</h3>
+    <h3>{value} from component {comp() + 8}</h3>   //using curly brackets we can use js like function (variables or functions or other js parts) inside the html
   )
+}
+const comp = () => {
+  return 5;
 }
